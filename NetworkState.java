6688ab1211a7b0897330a79c20b306958db56bf9
Hamidreza Zangoie <50046477+hamidsnet@users.noreply.github.com>
2019-08-06 13:23:35 +0430

    Update and rename NetworkState.txt to NetworkState.java

--- NetworkState.java
+++ NetworkState.java
@@ -1,7 +1,3 @@
-import android.content.Context;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
-
 public class NetworkState {
 
     public boolean isNetworkConnected(Context ctx) {
